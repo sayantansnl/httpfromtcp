c575adf67214d1d0681f599d3335c2a324423f52
sayantansnl <sayantansanyal72@gmail.com>
2026-01-28 13:18:15 +0530

    minor refactoring

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -16,37 +17,33 @@ func main() {
 		fmt.Printf("\nerror in opening file: %v", err)
 	}
 
-	fileInfo, err := file.Stat()
-	if err != nil {
-		fmt.Printf("\nerror getting file info: %v", err)
-	}
-
-	fileSize := fileInfo.Size()
 	defer file.Close()
 
 	fmt.Println("======Reading Data======")
 
 	currentLine := ""
 
-	for fileSize > 0 {
+	for {
 		buffer := make([]byte, 8)
-		numBytes, err := file.Read(buffer)
+		n, err := file.Read(buffer)
 		if err != nil {
-			log.Fatalf("\nerror in reading files: %v", err)
-			break
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			log.Fatalf("error in reading files: %v", err)
 		}
-		parts := strings.Split(string(buffer), "\n")
+		parts := strings.Split(string(buffer[:n]), "\n")
 		for i := 0; i < len(parts)-1; i++ {
 			fmt.Printf("read: %s\n", currentLine+parts[i])
 			currentLine = ""
 		}
 		currentLine += parts[len(parts)-1]
-		fileSize -= int64(numBytes)
-		if err == io.EOF {
-			break
-		}
 	}
 	if len(currentLine) > 0 {
 		fmt.Printf("read: %s\n", currentLine)
 	}
 }
+
+// func getLinesChannel(f io.ReadCloser) <-chan string {
+// 	f.Read()
+// }
