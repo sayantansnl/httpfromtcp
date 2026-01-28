af07c89326ffccf2d823a42bef963aa2cb454411
sayantansnl <sayantansanyal72@gmail.com>
2026-01-28 22:44:47 +0530

    goroutines enabled

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"log"
 	"os"
 	"strings"
 )
@@ -17,33 +16,44 @@ func main() {
 		fmt.Printf("\nerror in opening file: %v", err)
 	}
 
-	defer file.Close()
-
 	fmt.Println("======Reading Data======")
 
+	channel := getLinesChannel(file)
+
+	for line := range channel {
+		fmt.Printf("read: %s\n", line)
+	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	channel := make(chan string)
+	go receiveData(channel, f)
+	return channel
+}
+
+func receiveData(ch chan string, f io.ReadCloser) {
 	currentLine := ""
+	defer f.Close()
 
 	for {
 		buffer := make([]byte, 8)
-		n, err := file.Read(buffer)
+		n, err := f.Read(buffer)
 		if err != nil {
 			if errors.Is(err, io.EOF) {
 				break
 			}
-			log.Fatalf("error in reading files: %v", err)
+			fmt.Errorf("error in reading files, %w", err)
+			return
 		}
 		parts := strings.Split(string(buffer[:n]), "\n")
 		for i := 0; i < len(parts)-1; i++ {
-			fmt.Printf("read: %s\n", currentLine+parts[i])
+			ch <- currentLine + parts[i]
 			currentLine = ""
 		}
 		currentLine += parts[len(parts)-1]
 	}
 	if len(currentLine) > 0 {
-		fmt.Printf("read: %s\n", currentLine)
+		ch <- currentLine
 	}
+	close(ch)
 }
-
-// func getLinesChannel(f io.ReadCloser) <-chan string {
-// 	f.Read()
-// }
