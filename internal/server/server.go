333179a3663e1c6d82a5accbad1e498536bb0ee5
sayantansnl <sayantansanyal72@gmail.com>
2026-02-02 11:16:25 +0530

    server interface implemented

---
+++ internal/server/server.go
@@ -0,0 +1,52 @@
+package server
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"sync/atomic"
+)
+
+type Server struct {
+	listener net.Listener
+	closed   atomic.Bool
+}
+
+func Serve(port int) (*Server, error) {
+	portString := fmt.Sprintf(":%d", port)
+	l, err := net.Listen("tcp", portString)
+	if err != nil {
+		return nil, fmt.Errorf("unable to listen on port %s, error: %w", portString, err)
+	}
+	server := &Server{
+		listener: l,
+	}
+	server.closed.Store(false)
+
+	go server.listen()
+	return server, nil
+}
+
+func (s *Server) Close() error {
+	s.closed.Store(true)
+	return s.listener.Close()
+}
+
+func (s *Server) listen() {
+	for {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			s.closed.Store(true)
+			break
+		}
+		go s.handle(conn)
+	}
+}
+
+func (s *Server) handle(conn net.Conn) {
+	_, err := conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nHello World!"))
+	if err != nil {
+		log.Fatalf("cannot write to connection, error: %v", err)
+	}
+	conn.Close()
+}
